6e4e7bdf93959f8c06c6a547f72b0779bcc8401b
PepeLala <ppmauro@hotmail.com>
2014-04-06 12:48:46 -0300

    Agrego cosas nuevas sobre el libro Land Of Lisp

---
+++ land-of-lisp/prueba.lisp
@@ -0,0 +1,30 @@
+(defparameter *wizard-nodes* '((living-room (you are in the living-room. a wizard is snoring loudly on the couch.))
+			(garden (you are in a beautifull garden. there is a well in front of you.))
+			(attic (you are in the attic. there is a ginat welding torch in the corner.))))
+
+(defparameter *wizard-edges* '((living-room (garden west door)
+			 (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+(defparameter *max-label-length* 30)
+
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+	(if (> (length s) *max-label-length*)
+	    (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+	    s))
+      ""))
+
+(defun nodes->dot (nodes)
+  (mapc (lambda (node)
+	  (fresh-line)
+	  (princ (dot-name (car node)))
+	  (princ "[label=\"")
+	  (princ (dot-label node))
+	  (princ "\"];"))
+	nodes))
