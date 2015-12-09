05341d88ead841e5ab687933445934edca2b4ac2
Pedro M <pedro@localhost.localdomain>
2015-12-09 00:32:31 -0500

    comienzo con skill-sharing

--- eloquent-javascript/chapter-20/public-space/main-script.js
+++ eloquent-javascript/chapter-20/public-space/main-script.js
@@ -36,11 +36,15 @@ document.body.appendChild(elm("div", {},
 			      elm("h2", {id: "file-name"}, "File name"),
 			      elm("textarea", {rows: "20", cols: "100"}),
 			      elm("div", {},
-				  elm("button", {id: "save"}, "Save"),
+				  elm("button", {id: "save"}, "Save File"),
 				  elm("button", {id: "open"}, "Open File"),
-				  elm("button", {id: "close"}, "Delete")),
+				  elm("button", {id: "close"}, "Delete File"),
+				  elm("button", {id: "enter"}, "Enter Directory"),
+				  elm("button", {id: "exit"}, "Exit Direcotyr"),
+				  elm("span", {id: "error", style: "color: red"})),
 			      elm("div", {id: "listing"})));
 
+var directoryDepth = "";
 var selected = null;
 var fileName = document.querySelector("#file-name");
 var textarea = document.querySelectorAll("textarea")[0];
@@ -72,7 +76,7 @@ request("GET", "http://localhost:8000/.", function(text, error) {
 
 var openButton = document.querySelector("#open");
 openButton.addEventListener("click", function() {
-  request("GET", "http://localhost:8000/" + selected, function (text, error) {
+  request("GET", "http://localhost:8000/" + directoryDepth + selected, function (text, error) {
     if (error)
       console.log("Error...");
     else {
@@ -84,7 +88,7 @@ openButton.addEventListener("click", function() {
 
 var deleteButton = document.querySelector("#close");
 deleteButton.addEventListener("click", function() {
-  // request("DELETE", "http://localhost:8000/" + selected, function(_, error) {
+  // request("DELETE", "http://localhost:8000/" + directoryDepth + selected, function(_, error) {
   //   if (error)
   //     console.log("Error deleting file(" + selected + ": " + String(error));
   // });
@@ -93,10 +97,50 @@ deleteButton.addEventListener("click", function() {
 
 var saveButton = document.querySelector("#save");
 saveButton.addEventListener("click", function() {
-  request("PUT", "http://localhost:8000/" + selected, function(_, error) {
+  request("PUT", "http://localhost:8000/" + directoryDepth + selected, function(_, error) {
     if (error)
       console.log("Error writing file (" + selected + "): " + String(error));
     else
       window.location.reload(true);
   }, textarea.value);
 });
+
+var openDirButton = document.querySelector("#enter");
+openDirButton.addEventListener("click", function() {
+  request("GET", "http://localhost:8000/" + directoryDepth + selected, function(text, error) {
+    if (error)
+      console.log("Error entering to directory (" + directoryDepth + selected + "):" + String(error));
+    else {
+      var listing = document.querySelector("#listing");
+      listing.removeChild(listing.firstChild);
+      
+      var files = text.split("\n");
+      var list = elm("ul", {});
+      list.addEventListener("click", function(event) {
+	Array.prototype.forEach.call(list.childNodes, function(item) {
+	  if (item == event.target) {
+	    item.setAttribute("style", "background-color: violet");
+	    selected = item.textContent;
+	  } else {
+	    item.setAttribute("style", "background-color: none");
+	  }
+	});
+      });
+      files.forEach(function(file) {
+	var item = elm("li", {}, file);
+	list.appendChild(item);
+      });
+      document.querySelector("#listing").appendChild(list);
+      directoryDepth += selected;
+    }      
+  });
+});
+
+var exitButton = document.querySelector("#exit");
+exitButton.addEventListener("click", function() {
+  console.log("Se quiere salir de un directorio");
+  // hacer algo con las REGEXP para obtener la ultima carpeta y eliminarla del path
+});
+
+
+// TODO: meter todos los directorydepth + selected en una funcion que devuelva el path actual
