7dcefdfe49791e31db1c5957f22cc8e4311bb086
Pedro M <pedro@localhost.localdomain>
2015-12-15 21:59:51 -0500

    hecho el html, y empiezo con el codigo del cliente

---
+++ eloquent-javascript/skill-sharing/public/skillsharing_client.js
@@ -0,0 +1,14 @@
+function request(options, callback) {
+  var req = new XMLHttpRequest();
+  req.open(options.method || "GET", options.pathname, true);
+  req.addEventListener("load", function() {
+    if (req.status < 400)
+      callback(null, req.responseText);
+    else
+      callback(new Error("Request failed: " + req.statusText));
+  });
+  req.addEventListener("error", function() {
+    callback(new Error("Network error"));
+  });
+  req.send(options.body || null);
+}
